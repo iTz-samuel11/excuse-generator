0ebae788e0a227b5670e882a9dfe6e0dfb70b250
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-05-04 23:28:59 +0000

    finalizacion de trabajo de excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#excusa").innerHTML = generarExcusa();
   console.log("Hello Rigo from the console!");
+};
+
+let generarExcusa = () => {
+  let quien = ["mi perro", "mi abuela", "la tortuga", "mi pajaro"];
+  let verbo = ["se comio", "boto", "aplasto", "rompio"];
+  let que = ["mi tarea", "las llaves", "el carro", "mi telefono"];
+  let cuando = [
+    "antes de clase",
+    "antes de dormir",
+    "cuando termine",
+    "durante el almuerzo",
+    "mientras rezaba"
+  ];
+
+  let quienIndex = Math.floor(Math.random() * quien.length);
+  let verboIndex = Math.floor(Math.random() * verbo.length);
+  let queIndex = Math.floor(Math.random() * que.length);
+  let cuandoIndex = Math.floor(Math.random() * cuando.length);
+
+  return (
+    quien[quienIndex] +
+    " " +
+    verbo[verboIndex] +
+    " " +
+    que[queIndex] +
+    " " +
+    cuando[cuandoIndex]
+  );
 };
